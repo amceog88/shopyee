17abbf761be1ebf02b2d835b3afd62d38edc9ac1
Siegmund <amceog88@gmail.com>
2018-03-01 10:46:02 +0800

    create view for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root "products#index"
+  end
+
 end
